1806615255af77b76f81c97bbe24b8b62dbfac3b
Blake Porter <blake.porter@ibm.com>
2020-03-17 13:08:35 -0500

    Fundamentals Of JavaScript

---
+++ Fundamentals/Variables.js
@@ -0,0 +1,9 @@
+// var, let, const
+
+var name = "Shaggy Rogers"; // Initializes a variable called name with the value of "Mary Jane"
+console.log(name); // prints out the value of the "name" variable
+// "var" and "let" all you to reassign variables after they have been declared
+name = "Scooby Doo"; // name variable has been given a new value of "scooby doo"
+console.log(name):
+
+//variables can NOT start with a number
