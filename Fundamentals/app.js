65723748e89adb343582a38ce2d48f69b061913d
Blake Porter <blake.porter@ibm.com>
2020-03-17 12:55:37 -0500

    Fundamentals Of JavaScript

---
+++ Fundamentals/app.js
@@ -0,0 +1,21 @@
+//Displays "Hello World" In an Alert Box when you open the browser pagec
+alert("Hello World")
+// Logs to console from application
+console.log("Hello World");
+console.log(true);
+console.log(123):
+var greeting = 'hello'; // variables in javascript have to be declared with var
+console.log(greeting); // logs the greeting variable in the console
+console.log([1,2,3,4]); // logs an array with numbers 1 2 3 4
+console.log({a:1, b:2}): // Logs an object literal
+console.table({a:1, b:2}); // logs a table
+
+console.error("Error"); //logs an error in console, error appears in red
+console.clear(); //clears the console
+console.warn("This is a warning"); // logs a warning in console, appears in yellow
+console.time("Multiple lines"); //logs the time it takes to execute the code below
+  console.log("a");
+  console.log("b");
+  console.log("c");
+  console.log("d");
+console.timeEnd("Multiple lines") // ^^^^
