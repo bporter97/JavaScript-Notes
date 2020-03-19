2007e4b0ae78706bdcbcd36fa03278b3a21e717e
Blake Porter <49182700+bporter97@users.noreply.github.com>
2020-03-19 14:16:32 -0500

    Updated Data Types and variables.js

---
+++ Fundamentals/DataTypes.js
@@ -0,0 +1,23 @@
+// There are primitive Data Types and Reference Data Types in Javascript.
+// Primitive Data types, data is stored directly in the location that the variable accesses.
+// Reference Data types: Data is Accessed by references; there is a pointer that points to a location in memory.
+// Primitive Data types include: Strings, numbers, booleans, null, undefined and symbols.
+// Reference Data Types include: Arrays, object literals, functions and dates.
+// Types are associated with values, not variables, and the same variable can hold multiple Types.
+// Javascript is dynamically typed.
+
+// Strings
+const name = 'John Doe';
+console.log(typeof name) //typeof: Allows you to see the type of data that is stored in a variable
+
+//numbers
+const age = 32;
+
+//Boolean
+const isTrue = True;
+
+//null
+const movie = null
+
+//undefined
+let test;
--- Fundamentals/Variables.js
+++ Fundamentals/Variables.js
@@ -10,3 +10,13 @@ console.log(name):
 
 // CONST: Stands for constant, which means it can change or be reassigned
 // When using const, you HAVE to declare a value,
+const company = { //in this case, the constant is the class called company
+  companyName: "Mystery INC" //however, the attributes inside the class can be re-named
+}
+console.log(company.companyName);
+company.comanyName = "Mystery Incorporated"; //Here we assigned a new value to the companyName Attribute in the company class
+
+const numbers = [1,2,3,4,5] // we can update arrays as well using the .push method, however we can't declare a new array
+// variable with the name numbers that has 1-6 in it.
+// Same goes for objects
+// Values cannot be reassigned as a new primitive value when they are delcared with const 
